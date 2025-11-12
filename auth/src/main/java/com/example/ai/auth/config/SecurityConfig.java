2ec6352b28361c729b718a02ebcc942d3cc8d091
Jie <961334527@qq.com>
2025-11-12 19:22:40 +0800

    添加未认证统一处理和日志配置

--- auth/src/main/java/com/example/ai/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/example/ai/auth/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.example.ai.auth.config;
 
+import com.alibaba.fastjson2.JSONObject;
 import com.example.ai.auth.filter.JwtTokenFilter;
+import com.example.ai.common.model.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,6 +16,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -42,8 +46,21 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated()
                 )
+                .exceptionHandling((ex) -> ex.authenticationEntryPoint(authenticationEntryPoint())) // 未登录处理
                 .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setContentType("application/json;charset=utf-8");
+            ApiResult<String> result = new ApiResult<>();
+            result.setCode(HttpStatus.UNAUTHORIZED.value());
+            result.setMsg("请登录！");
+            result.setData(request.getRequestURI());
+            response.getWriter().write(JSONObject.toJSONString(result));
+        };
     }
 }
