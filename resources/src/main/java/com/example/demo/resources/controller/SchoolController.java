4824b4e0f39830eee6a2371b7856068b2c4316b7
Jie <961334527@qq.com>
2025-11-14 19:59:08 +0800

    重构项目包名和模块路径结构

--- resources/src/main/java/com/example/demo/resources/controller/SchoolController.java
+++ resources/src/main/java/com/example/demo/resources/controller/SchoolController.java
@@ -2,7 +2,6 @@ package com.example.demo.resources.controller;
 
 import com.example.demo.common.model.ApiResult;
 import com.example.demo.common.model.PageResult;
-import com.example.ai.resources.model.dto.*;
 import com.example.demo.resources.model.dto.SchoolDTO;
 import com.example.demo.resources.model.dto.SchoolQueryDTO;
 import com.example.demo.resources.model.dto.SchoolSaveDTO;
@@ -11,7 +10,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
